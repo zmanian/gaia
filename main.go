5432399faf4c2d4078b491c16d6063288dcbb5c5
Matt Bell <mappum@gmail.com>
2017-06-21 09:53:11 -0700

    Register delegationgame plugin in main fow now

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"os"
 
+	dg "github.com/cosmos/basecoin-delegationgame"
 	"github.com/spf13/cobra"
-
-	// import _ to register the stake plugin to apptx
+	// _ "github.com/tendermint/basecoin-delegationgame/commands"
 	_ "github.com/tendermint/basecoin-stake/cmd/stakecoin/commands"
 	"github.com/tendermint/basecoin/cmd/commands"
+	"github.com/tendermint/basecoin/types"
 	"github.com/tendermint/tmlibs/cli"
 )
+
+func init() {
+	commands.RegisterStartPlugin("delegationGame",
+		func() types.Plugin {
+			return &dg.Plugin{}
+		},
+	)
+}
 
 func main() {
 	var RootCmd = &cobra.Command{
